39599108a5fa599089b931037a9656237ef8cda7
SergSmilya <sergsmilichenko@gmail.com>
2023-03-23 10:21:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,63 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import Input from './Input/Input';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const baseContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  componentDidMount() {
-    const parsedData = JSON.parse(localStorage.getItem('contacts'));
-    if (parsedData.length !== 0) {
-      this.setState({ contacts: parsedData });
-    }
+export function App() {
+  const parsedData = JSON.parse(localStorage.getItem('contacts'));
+
+  const [contacts, setContacts] = useState(parsedData ?? baseContacts);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  function updateState(values) {
+    setContacts(prev => [...prev, values]);
   }
 
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
+  function onDeleteClick(id) {
+    setContacts(prev => prev.filter(ob => ob.id !== id));
   }
 
-  updateState = values => {
-    this.setState(({ contacts }) => ({ contacts: [values, ...contacts] }));
-  };
-
-  onDeleteClick = id => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(ob => ob.id !== id),
-    }));
-  };
-
-  checkName = name => {
-    return this.state.contacts.find(ob => name === ob.name);
-  };
-
-  onFilterChange = e => {
-    this.setState({
-      filter: e.target.attributes.name.ownerElement.value.toLowerCase(),
-    });
-  };
-
-  onFilter = () => {
-    return this.state.contacts.filter(({ name }) =>
-      name.toLowerCase().includes(this.state.filter)
-    );
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'block',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-
-        <Input updateState={this.updateState} checkName={this.checkName} />
-
-        <Filter onFilterChange={this.onFilterChange} />
-
-        <Contacts
-          onDeleteClick={this.onDeleteClick}
-          contacts={this.onFilter()}
-        />
-      </div>
-    );
+  function checkName(name) {
+    return contacts.find(ob => name === ob.name);
   }
+
+  function onFilterChange(e) {
+    setFilter(e.target.attributes.name.ownerElement.value.toLowerCase());
+  }
+
+  function onFilter() {
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
+  }
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'block',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+
+      <Input updateState={updateState} checkName={checkName} />
+
+      <Filter onFilterChange={onFilterChange} />
+
+      <Contacts onDeleteClick={onDeleteClick} contacts={onFilter()} />
+    </div>
+  );
 }
